c571f47049653ab091218094ca94d07f629980cf
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-07-19 21:12:13 -0400

    add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  computerChoice = Math.floor(Math.random() * 3)
+  switch(computerChoice) {
+    case 0:
+      return "Rock"
+    case 1:
+      return "Paper"
+    case 2:
+      return "Scissors"
+  }
+}
