1ea7d4e4eaff115af90a50b106265cc1aa184542
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-07-24 16:03:35 -0400

    add stylesheet and make minor tweaks for proper layout

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const computerScoreDOM = document.querySelector(".computerScore")
 const finalScore = document.querySelector(".finalScore")
 const body = document.body
 const reset = document.querySelector(".reset")
+const myChoice = document.querySelector(".myChoice")
+const computerChoiceDOM = document.querySelector(".computerChoice")
 
 let humanChoice = ""
 rockButton.addEventListener("click", rock = function() {
@@ -45,36 +47,50 @@ function playRound(computerChoice) {
     output.textContent = `Draw computer selected ${computerChoice}`
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     humanScore++
     output.textContent = "You win! rock beats scissors"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++
     output.textContent = "You lose! paper beats rock"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore++
     output.textContent = "You lose! scissors beat paper"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++
     output.textContent = "You win! paper beats rock"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++
     output.textContent = "You lose! rock beats scissors"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     humanScore++
     output.textContent = "You win! scissors beat paper"
     humanScoreDOM.textContent = `Your Score: ${humanScore}`
     computerScoreDOM.textContent = `Computer Score: ${computerScore}`
+    myChoice.textContent = humanChoice
+    computerChoiceDOM.textContent = computerChoice
   }
   if (humanScore > 4) {
     finalScore.textContent = `YOU WIN!`
